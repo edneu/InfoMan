a8c3cc8915fecc19df32182fdd4fbc22d89825a2
Neu <edneu@ufl.edu>
2022-08-30 05:53:05 -0400

    Update 2022-08-30

--- Finance/Payroll Cross Match.sql
+++ Finance/Payroll Cross Match.sql
@@ -1,9 +1,9 @@
 ## Payroll MAtch
-drop table work.payrollmatchaug22;
+drop table work.payrollmatchaug26;
 
 DROP TABLE IF EXISTS work.pymatch ;
 Create table work.pymatch AS
-Select * from work.payrollmatchaug22;
+Select * from work.payrollmatchaug26;
 
 SET SQL_SAFE_UPDATES = 0;
 delete from work.pymatch WHERE UFID IS NULL;
@@ -35,7 +35,7 @@ ALTER TABLE work.pyxmatch
 	ADD CTSI int(1),
 	ADD SECIM int(1),
 	ADD MD_PHD int(1),
-	ADD OCR int(1),
+	##ADD OCR int(1),
 	ADD CERHB int(1),
     ##ADD NETWORK int(1),
     ADD On_Payroll int(3);
@@ -47,7 +47,7 @@ SET ASSIST=0,
 	CTSI=0,
 	SECIM=0,
 	MD_PHD=0,
-	OCR=0,
+	##OCR=0,
 	CERHB=0,
     ##NETWORK=0,
     On_Payroll=0; 
@@ -56,11 +56,11 @@ UPDATE work.pyxmatch pm, work.pymatch lu SET ASSIST=1 WHERE pm.UFID=lu.UFID AND 
 UPDATE work.pyxmatch pm, work.pymatch lu SET CTSI=1 WHERE pm.UFID=lu.UFID AND lu.Source="CTSI";
 UPDATE work.pyxmatch pm, work.pymatch lu SET SECIM=1 WHERE pm.UFID=lu.UFID AND lu.Source="SECIM";
 UPDATE work.pyxmatch pm, work.pymatch lu SET MD_PHD=1 WHERE pm.UFID=lu.UFID AND lu.Source="MD_PHD";
-UPDATE work.pyxmatch pm, work.pymatch lu SET OCR=1 WHERE pm.UFID=lu.UFID AND lu.Source="OCR";
+##UPDATE work.pyxmatch pm, work.pymatch lu SET OCR=1 WHERE pm.UFID=lu.UFID AND lu.Source="OCR";
 UPDATE work.pyxmatch pm, work.pymatch lu SET CERHB=1 WHERE pm.UFID=lu.UFID AND lu.Source="CERHB";
 ##UPDATE work.pyxmatch pm, work.pymatch lu SET NETWORK=1 WHERE pm.UFID=lu.UFID AND lu.Source="NETWORK";
-UPDATE work.pyxmatch SET On_Payroll= CTSI+SECIM+MD_PHD+OCR+CERHB;
-##UPDATE work.pyxmatch SET On_Payroll= 1 WHERE (CTSI+SECIM+MD_PHD+OCR+CERHB)>0;
+UPDATE work.pyxmatch SET On_Payroll= CTSI+SECIM+MD_PHD+CERHB;
+
 
 select *  from work.pyxmatch;
 
