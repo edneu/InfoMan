97911618d9a8b403e3f2bd2c1d38c7cf5969cd06
Ed Neu <edneu@ufl.edu>
2022-09-16 15:49:53 -0400

    Update V Drive  2022-09-16 CTSI

--- Utility/Table Lists.sql
+++ Utility/Table Lists.sql
@@ -13,18 +13,63 @@ SELECT table_name AS 'Table',
 ROUND(((data_length + index_length) / 1024 / 1024), 2) AS 'Size (MB)',
 UPDATE_TIME
 FROM information_schema.TABLES
-WHERE table_schema = "Adhoc"
+WHERE table_schema NOT IN "Adhoc"
 ORDER BY (data_length + index_length) DESC;
 
 
 desc information_schema.TABLES;
 
 ### FILE LIST BY SIZE
+
+DROP TABLE IF EXISTS  work.TableList;
+CREATE TABLE work.TableList AS
 SELECT 
 TABLE_SCHEMA AS 'Schema',
 table_name AS 'Table',
 ROUND(((data_length + index_length) / 1024 / 1024), 2) AS 'Size (MB)'
 FROM information_schema.TABLES
+WHERE TABLE_SCHEMA NOT IN ('mysql','information_schema','sys','performance_schema')
+ORDER BY ROUND(((data_length + index_length) / 1024 / 1024), 2)  DESC;
+
+#######################
+select "lookup" as DB, min(FUNDS_ACTIVATED) MinDate, MAX(FUNDS_ACTIVATED) as MaxDate from lookup.awards_history
+UNION ALL
+select "loaddata" as DB, min(FUNDS_ACTIVATED) MinDate, MAX(FUNDS_ACTIVATED) as MaxDate from loaddata.awards_history;
+
+
+Select count(*) from lookup.roster;
+Select count(*) from loaddata.newroster;
+
+#######################################################
+## DROP FROM 9/16/2022
+/*
+DROP TABLE ctsi_survey.LongWeight;
+DROP TABLE ctsi_survey.LongWeightbackup;
+DROP TABLE finance.DetailAssn;
+DROP TABLE finance.Detail;
+DROP TABLE finance.detail;
+DROP TABLE loaddata.awards_history;
+DROP TABLE loaddata.newtranshist202209;
+DROP TABLE loaddata.newtranshist202208;
+DROP TABLE loaddata.newtranshist202207;
+DROP TABLE loaddata.newtranshist202206;
+DROP TABLE loaddata.newtranshist202205;
+DROP TABLE loaddata.newroster;
+DROP TABLE loaddata.roster;
+DROP TABLE loaddata.q1q2_2022_roster;
+DROP TABLE lookup.ct_gov;
+DROP TABLE work.EmployeeUpdate;
+DROP TABLE work.Employees;
+*/
+
+
+
+
+
+#########################
+
+
+
 #WHERE table_schema = "space"
 ORDER BY (data_length + index_length) DESC;
 
