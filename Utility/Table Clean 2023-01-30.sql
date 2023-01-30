f472d3461d31072a25c361761f000cebc3f41f12
Neu <edneu@ufl.edu>
2023-01-30 10:23:08 -0500

    Update 2023-01-30

---
+++ Utility/Table Clean 2023-01-30.sql
@@ -0,0 +1,11 @@
+DROP TABLE IF EXISTS loaddata.program_metrics;
+DROP TABLE IF EXISTS loaddata.activeemp20221031;
+DROP TABLE IF EXISTS loaddata.newtranshist202211;
+DROP TABLE IF EXISTS loaddata.newtranshist202210;
+DROP TABLE IF EXISTS loaddata.newtranshist;
+
+DROP TABLE IF EXISTS Adhoc.comb_hist_report202211BU;
+DROP TABLE IF EXISTS Adhoc.comb_hist_report202210BU;
+DROP TABLE IF EXISTS Adhoc.comb_hist_report202209BU;
+DROP TABLE IF EXISTS Adhoc.MattOutALL;
+DROP TABLE IF EXISTS loaddata.page1;
